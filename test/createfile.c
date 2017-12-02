f87ef65ba7e4d10db9bc0fcd8ba673d6d8baa3b2
annguyen011197 <tranminhannguyen@gmail.com>
2017-12-02 08:58:58 +0700

    createfile v1

--- test/createfile.c
+++ test/createfile.c
@@ -5,8 +5,10 @@
 int 
 main(){
     int len;
-    char filename[maxlen+1];
-    if(Create("text_1.txt")==-1){
+    char *filename[maxlen+1];
+    PrintfString("Input file's name:");
+    ReadString(filename,maxlen);
+    if(Create(filename)==-1){
         PrintString("Error when create file");
     }else{
         PrintString("Create file succesfull");
