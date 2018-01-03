10bf74ddff769d849da76a0a2ec326a22e632b27
annguyen011197 <tranminhannguyen@gmail.com>
2018-01-03 23:11:06 +0700

    da3 1

--- userprog/sTable.cc
+++ userprog/sTable.cc
@@ -3,6 +3,35 @@
 #include "thread.h"
 #include "addrspace.h"
 
+Sem::Sem(char* Name, int init){
+    if(name==NULL || init < 0) {
+        this->name = "";
+        sem = NULL;
+    }
+    strcpy(this->name,Name);
+    sem = new Semaphore(this->name,init);
+}
+
+Sem::~Sem(){
+    delete sem;
+}
+
+void Sem::wait(){
+    sem->P();
+}
+
+void Sem::signal(){
+    sem->V();
+}
+
+char* Sem::getName(){
+    return name;
+}
+
+Semaphore * Sem::getSem(){
+    return this->sem;
+}
+
 sTable::sTable()
 {
     bm = new BitMap(10);
--- userprog/sTable.h
+++ userprog/sTable.h
@@ -8,6 +8,21 @@
 
 #define MAX_SEMAPHORE 10
 
+class Sem
+{
+  private:
+    char name[50];
+    Semaphore *sem;
+
+  public:
+    Sem(char *Name, int init);
+    ~Sem();
+    void wait();
+    void signal();
+    char *getName();
+    Semaphore * getSem();
+};
+
 class sTable
 {
   private:
